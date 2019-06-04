357c86af9e47270b71303c4bd1156ba17c6e820c
Sabrina Heid <sabrina.heid@sbb.ch>
2019-06-04 08:34:47 +0200

    adding download navi logic

--- web/public/js/check_login.js
+++ web/public/js/check_login.js
@@ -4,18 +4,21 @@ firebase.auth().onAuthStateChanged(function(user) {
 			var email = firebase.auth().currentUser.email;
 			console.log(uid + ' ' + email);
 			$('.main').addClass('isLoggedIn');
-			$('.nav-item-download').show();
+
+			$('.navbar-nav').append('<li class="nav-item nav-item-download"><a class="nav-link" href="download.html">Download</a></li>');
 	} else {
 		$('.main').removeClass('isLoggedIn');
-		$('.nav-item-download').hide();
+
+		if ($('.nav-item-download').length) {
+			$('.nav-item-download').remove();
+		}
 	}
 
 	$('#sign-out-button').click(function() {
 		firebase.auth().signOut();
+		
 		$('.main').removeClass('level-1');
 		$('.main').removeClass('level-2');
 		$('.main').removeClass('level-3');
-
-		$('.nav-item-download').hide();
 	});
 });
