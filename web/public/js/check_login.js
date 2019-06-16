746d7edf802743c51a3c6a1214df6ff00b704f6d
Sabrina Heid <sabrina.heid@sbb.ch>
2019-06-16 17:58:11 +0200

    change login logic

--- web/public/js/check_login.js
+++ web/public/js/check_login.js
@@ -1,37 +1,45 @@
 firebase.auth().onAuthStateChanged(function(user) {
 	if (user) {	
-			// var uid = firebase.auth().currentUser.uid;
-			// var email = firebase.auth().currentUser.email;
-			// console.log(uid + ' ' + email);
+		
+
 		$('.main').addClass('isLoggedIn');
-		$('.navbar-nav').append('<li class="nav-item nav-item-download"><a class="nav-link" href="download.html">Download</a></li>');
+		if (localStorage.level !== undefined) {
+			$('.navbar-nav').append('<li class="nav-item nav-item-download"><a class="nav-link" href="download.html">Download</a></li>');
+		}
+		
 
 		var uid = firebase.auth().currentUser.uid;
 
 		var update = database.ref('/users/' + uid);
 		update.on('value', function(snapshot) {
 			var data = snapshot.val();
+
+			if (data !== null) {
+				if (data.level === '1') {
+					localStorage.level = '1';
+					// location.reload();
+					// $('.main').addClass('level-1');
+					// $('.main').removeClass('level-2');
+					// $('.main').removeClass('level-3');
+				}
+
+				if (data.level === '2') {
+					localStorage.level = '2';
+					// location.reload();
+					// $('.main').addClass('level-2');
+					// $('.main').removeClass('level-1');
+					// $('.main').removeClass('level-3');
+				}
+
+				if (data.level === '3') {
+					localStorage.level = '3';
+					// location.reload();
+					// $('.main').addClass('level-3');
+					// $('.main').removeClass('level-1');
+					// $('.main').removeClass('level-2');
+				}
+			}
 			
-			if (data.level === '1') {
-				localStorage.level = '1';
-				// $('.main').addClass('level-1');
-				// $('.main').removeClass('level-2');
-				// $('.main').removeClass('level-3');
-			}
-
-			if (data.level === '2') {
-				localStorage.level = '2';
-				// $('.main').addClass('level-2');
-				// $('.main').removeClass('level-1');
-				// $('.main').removeClass('level-3');
-			}
-
-			if (data.level === '3') {
-				localStorage.level = '3';
-				// $('.main').addClass('level-3');
-				// $('.main').removeClass('level-1');
-				// $('.main').removeClass('level-2');
-			} 
 		});
 	} else {
 		$('.main').removeClass('isLoggedIn');
@@ -45,7 +53,7 @@ firebase.auth().onAuthStateChanged(function(user) {
 	$('#sign-out-button').click(function() {
 		firebase.auth().signOut();
 		localStorage.removeItem('level');
-		location.reload();
+		window.location = '/index.html'; 
 		// $('.main').removeClass('level-1');
 		// $('.main').removeClass('level-2');
 		// $('.main').removeClass('level-3');
---
+++ web/public/js/download.js
@@ -0,0 +1,16 @@
+$('.download-button').click(function() {
+	
+
+	if (firebase.auth().currentUser) {
+		var uid = firebase.auth().currentUser.uid;
+		var email = firebase.auth().currentUser.email;
+		console.log(uid + email);
+		firebase.database().ref('users/' + uid).set({
+			email: email,
+			level: '1',
+			part: '2'
+		});
+
+		location.reload();
+	}
+});
