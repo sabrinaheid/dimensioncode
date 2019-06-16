89801bac0b2fc4239d78c424c3d7c18307c27f07
Sabrina Heid <sabrina.heid@sbb.ch>
2019-06-16 20:28:02 +0200

    add level 1

---
+++ web/public/js/puzzle_1.js
@@ -0,0 +1,9 @@
+if (localStorage.level == '1') {
+	$('.puzzle-1').click(function() {
+		$('.overlay-puzzle-1').show();
+	});
+
+	$('.overlay-puzzle-1').click(function() {
+		$('.overlay-puzzle-1').hide();
+	});
+}
