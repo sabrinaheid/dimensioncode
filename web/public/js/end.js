230d399161380306b1b3f75593d46ee940085b3f
Sabrina Heid <sabrina.heid@sbb.ch>
2019-07-19 20:50:25 +0200

    add end page

--- web/public/js/end.js
+++ web/public/js/end.js
@@ -1,11 +1,17 @@
 firebase.auth().onAuthStateChanged(function(user) {
 	if (user) {	
 		var uid = firebase.auth().currentUser.uid;
+		var email = firebase.auth().currentUser.email;
 
-		var starCountRef = database.ref('/users/' + uid);
-		starCountRef.on('value', function(snapshot) {
+		var db = database.ref('/users/' + uid);
+		db.on('value', function(snapshot) {
 
 			if(snapshot.val().level == 3 && snapshot.val().part == 3) {
+				firebase.database().ref('users/' + uid).set({
+					email: email,
+					level: '1',
+					part: '1'
+				});
 				window.location.href = 'end.html';
 			}
 		});
